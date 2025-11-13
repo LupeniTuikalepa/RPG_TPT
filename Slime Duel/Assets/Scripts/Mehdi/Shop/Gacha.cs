c939c02412de2f6633b0e182876eb4f56a5b1b54
Yuu-NiKorn <yuuyunikorn@gmail.com>
2025-11-13 14:42:38 +0100

    je push avant de refaire une connerie

--- Slime Duel/Assets/Scripts/Mehdi/Shop/Gacha.cs
+++ Slime Duel/Assets/Scripts/Mehdi/Shop/Gacha.cs
@@ -1,17 +1,39 @@
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-
+using Random = UnityEngine.Random;
 
 public class Gacha : MonoBehaviour
 {
-    [Header("proba: commun -> 40/100, rare -> 30/100, epic -> 20/100, legendaire -> 10/100")]
-    [SerializeField] public List<Skin> Commun_Skins = new List<Skin>();
-    [SerializeField] public List<Skin> Rare_Skins = new List<Skin>();
-    [SerializeField] public List<Skin> Epic_Skins = new List<Skin>();
-    [SerializeField] public List<Skin> Legendary_Skins = new List<Skin>();
+    private List<Skin> Commun_Skins = new List<Skin>();
+    private List<Skin> Rare_Skins = new List<Skin>();
+    private List<Skin> Epic_Skins = new List<Skin>();
+    private List<Skin> Legendary_Skins = new List<Skin>();
+    
     private int rarity_preOp = 0;
     private int rarity_pulled = 0;
+
+    private void Awake()
+    {
+        var skins = Resources.LoadAll<Skin>("Skins");
+        foreach (var skin in skins)
+        {
+            switch (skin.skinRarity)
+            {
+                case SkinRarity.Common:
+                    Commun_Skins.Add(skin);
+                    break;
+                case SkinRarity.Rare:
+                    Rare_Skins.Add(skin);
+                    break;
+                case SkinRarity.Epic:
+                    Epic_Skins.Add(skin);
+                    break;
+                case SkinRarity.Legendary:
+                    Legendary_Skins.Add(skin);
+                    break;
+            }
+        }
+    }
 
     public Skin pull()
     {
@@ -22,26 +44,22 @@ public class Gacha : MonoBehaviour
         int rarity_legendary = 10 * Legendary_Skins.Count;
         rarity_preOp = rarity_commun + rarity_rare + rarity_epic + rarity_legendary;
         rarity_pulled = Random.Range(1, rarity_preOp + 1);
+
         if (rarity_pulled <= rarity_commun)
-        {
             pulled_skin = Commun_Skins[Random.Range(0, Commun_Skins.Count)];
-            return pulled_skin;
-        }
-        if (rarity_pulled <= rarity_commun + rarity_rare)
-        {
+        else if (rarity_pulled <= rarity_commun + rarity_rare)
             pulled_skin = Rare_Skins[Random.Range(0, Rare_Skins.Count)];
-            return pulled_skin;
-        }
-        if (rarity_pulled <= rarity_commun + rarity_rare + rarity_epic)
-        {
+        else if (rarity_pulled <= rarity_commun + rarity_rare + rarity_epic)
             pulled_skin = Epic_Skins[Random.Range(0, Epic_Skins.Count)];
-            return pulled_skin;
-        }
-        if (rarity_pulled <= rarity_commun + rarity_rare + rarity_epic + rarity_legendary)
-        {
+        else
             pulled_skin = Legendary_Skins[Random.Range(0, Legendary_Skins.Count)];
-            return pulled_skin;
-        }
+
         return pulled_skin;
+    }
+    
+    public void PullButton()
+    {
+        Skin result = pull();
+        Debug.Log($"Tirage réussi : {result.name} ({result.skinRarity})");
     }
 }
--- Slime Duel/Assets/Scripts/Mehdi/Shop/Skin.cs
+++ Slime Duel/Assets/Scripts/Mehdi/Shop/Skin.cs
@@ -1,8 +1,15 @@
 using System.Net.Mime;
 using UnityEngine;
 
+public enum SkinRarity
+{
+    Common,
+    Rare,
+    Epic,
+    Legendary
+}
 public class Skin : MonoBehaviour
 {
     [SerializeField] private Sprite Visual;
-    [SerializeField] [Header("0 = commun, rare = 1, epic = 2, legendaire = 3")] public int rarity = 0; // commun = 0, rare = 1, epic = 2, legendaire = 3
+    [SerializeField] public SkinRarity skinRarity = 0; // commun = 0, rare = 1, epic = 2, legendaire = 3
 }
